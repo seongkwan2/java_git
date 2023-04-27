7e2d3a81adb80f3adc37a2942fb4e6df9fe215af
test123 <zaq3195@naver.com>
2023-04-27 13:03:07 +0900

    게시판기능추가

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,11 +2,14 @@ package main;
 
 import java.util.Scanner;
 
+import board.BoardService;
+
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int num;
 		
+		BoardService bo = new BoardService();
 		while(true) {
 			System.out.println("1.로그인 기능");
 			System.out.println("2.회원 기능");
@@ -18,6 +21,7 @@ public class MainClass {
 			case 2:
 				break;
 			case 3:
+				bo.board();
 				break;
 			}
 			
