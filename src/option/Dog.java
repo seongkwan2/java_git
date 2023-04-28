5ccb14f7e4cda2108a2e0f869b2595dc48f0108e
leeyestudyblog <lovewl1021@naver.com>
2023-04-28 13:29:30 +0900

    개

---
+++ src/option/Dog.java
@@ -0,0 +1,8 @@
+package option;
+
+public class Dog extends animal{
+ public void dog() {
+	 System.out.println("짖기");
+	 System.out.println("물기");
+ }
+}
--- src/option/MainClass01.java
+++ src/option/MainClass01.java
@@ -2,6 +2,8 @@ package option;
 
 public class MainClass01 {
 	public static void main(String[] args) {
+		Dog d = new Dog();
+		d.dog();
 		
 	}
 }
